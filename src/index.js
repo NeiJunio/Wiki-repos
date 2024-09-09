16cfb7cf9f7357cf83591769f3b93b16e4029f71
Nei Junio Nogueira Gomes <152228340+NeiJunio@users.noreply.github.com>
2024-09-08 23:28:31 -0300

    commit - projeto finalizado

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './pages/Home';
-
-import './styles.css';
+import App from './pages/App';
+import GlobalStyles from './styles'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
+        <GlobalStyles />
         <App />
     </React.StrictMode>
 );
